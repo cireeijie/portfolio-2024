c3c42dc8fb9d9241b209373bfaf3b948f8f5a3c8
Eric <ericjohnariate@gmail.com>
2024-08-02 16:06:30 +0800

    homepage update

---
+++ app/components/MainHeader.tsx
@@ -0,0 +1,38 @@
+import React from "react";
+
+export default function MainHeader() {
+  return (
+    <div className="flex justify-between items-center py-10">
+      <div className="w-[50px]">
+        <svg
+          className="w-[50px]"
+          width="1015"
+          height="auto"
+          viewBox="0 0 1015 1300"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            d="M147.02 8.6086C150.756 3.21682 156.899 0 163.459 0H995C1006.05 0 1015 8.95431 1015 20V195C1015 206.046 1006.05 215 995 215H42.1917C26.0545 215 16.5616 196.872 25.7529 183.609L147.02 8.6086Z"
+            fill="#D9D9D9"
+          />
+          <path
+            d="M799.805 656.609C799.805 651.469 801.784 646.525 805.333 642.805L980.528 459.139C992.984 446.08 1015 454.897 1015 472.943V962.891V1280C1015 1291.05 1006.05 1300 995 1300H799.805H21C9.95431 1300 1 1291.05 1 1280V1101.29C1 1090.24 9.9543 1081.29 21 1081.29H779.805C790.85 1081.29 799.805 1072.33 799.805 1061.29V962.891V656.609Z"
+            fill="#D9D9D9"
+          />
+          <path
+            d="M0 353C0 341.954 8.95431 333 20 333H456.5H865.407C883.126 333 892.094 354.339 879.693 366.996L708.13 542.113C704.368 545.952 699.219 548.116 693.843 548.116H271C259.954 548.116 251 557.071 251 568.116V726.454C251 737.499 259.954 746.453 271 746.453H682.248C693.294 746.453 702.248 755.408 702.248 766.453V942C702.248 953.046 693.294 962 682.248 962H215.127H20C8.95429 962 0 953.046 0 942L0 548.116V353Z"
+            fill="#D9D9D9"
+          />
+        </svg>
+      </div>
+      <div>
+        <div className="w-[50px] h-[50px] bg-white rounded-full py-2 px-3 flex flex-col gap-1 justify-center items-end cursor-pointer group">
+          <div className="w-[50%] group-hover:w-full h-[3px] bg-[#1A1A1A] transition-all"></div>
+          <div className="w-[75%] group-hover:w-full h-[3px] bg-[#1A1A1A] transition-all"></div>
+          <div className="w-full h-[3px] bg-[#1A1A1A]"></div>
+        </div>
+      </div>
+    </div>
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,112 +1,66 @@
+/* eslint-disable react/no-unescaped-entities */
+"use client";
+
+import Link from "next/link";
+import MainHeader from "./components/MainHeader";
+import Typewriter from "typewriter-effect";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
+    <main className="h-screen bg-[#1A1A1A] w-[80%] max-w-7xl mx-auto my-0 overflow-hidden relative">
+      <Image
+        className="absolute right-0 bottom-0 h-[85%] w-auto grayscale brightness-80"
+        src={"/portfolio-hero.png"}
+        alt="hero-bg"
+        width={1920}
+        height={1080}
+      />
+      <MainHeader />
+      <div className="h-screen flex">
+        <div className="pt-[15%]">
+          <h1 className="hidden">Eric John Ariate</h1>
+          <span className="text-[#3C3C3C] font-black text-[75px]">I'm a</span>
+          <div className="font-black text-[75px] leading-none">
+            <Typewriter
+              options={{
+                strings: ["developer", "designer"],
+                autoStart: true,
+                loop: true,
+              }}
             />
-          </a>
+          </div>
+          <p className="pt-8 text-[#858585] text-[18px] font-medium">
+            also a frustrated singer, dog lover, and a loving husband.
+          </p>
+          <Link href={"#"} className="flex gap-2 items-center pt-5 group w-fit">
+            <div className="w-[50px] h-[2px] bg-white group-hover:w-0 transition-all"></div>
+            <span>about me</span>
+            <svg
+              className="w-0 group-hover:w-[50px] transition-all"
+              width="42"
+              height="10"
+              viewBox="0 0 42 10"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M37 1L41 5L37 9"
+                stroke="white"
+                stroke-width="2"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+              />
+              <path
+                d="M1 5L41 5"
+                stroke="white"
+                stroke-width="2"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+              />
+            </svg>
+          </Link>
         </div>
-      </div>
-
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
       </div>
     </main>
   );
